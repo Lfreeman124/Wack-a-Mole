e39c24cde9816bfdf65491719c9875af3b13fdd1
Lauren Freeman <lfreeman124@gmail.com>
2022-02-06 13:11:26 +0100

    mole

--- wack.js
+++ wack.js
@@ -5,15 +5,15 @@ let moleScore = document.getElementById("mole-score");
 youScore.innerHTML = parseInt(0);
 moleScore.innerHTML = parseInt(0);
 
-let random1 = Math.floor(Math.random() * 50);
-let random2 = Math.floor(Math.random() * 50);
+let random1 = Math.floor(Math.random() * 20);
+let random2 = Math.floor(Math.random() * 20);
 mole.style.gridArea = `${random1} / ${random2} / ${random1 + 1} / ${
   random2 + 1
 }`;
 
 setInterval(function () {
-  let random1 = Math.floor(Math.random() * 50);
-  let random2 = Math.floor(Math.random() * 50);
+  let random1 = Math.floor(Math.random() * 20);
+  let random2 = Math.floor(Math.random() * 20);
   mole.style.gridArea = `${random1} / ${random2} / ${random1 + 1} / ${
     random2 + 1
   }`;
